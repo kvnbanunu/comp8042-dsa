38cf1c5f785e676ed47e30a5cb8ab16ab97406dc
kvnbanunu <kvnbanunu@gmail.com>
2026-01-29 11:26:56 -0800

    lab2: implemented shuffle

--- lab2/PermutationGenerator.java
+++ lab2/PermutationGenerator.java
@@ -7,6 +7,10 @@ class PermutationGenerator {
     this.size = n;
     this.inputList = new int[n];
     this.interList = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      this.inputList[i] = i;
+    }
   }
 
   public PermutationGenerator(int[] inputList) {
@@ -23,5 +27,13 @@ class PermutationGenerator {
     return new int[0];
   }
 
-  public void shuffle() {}
+  public void shuffle() {
+    this.interList = this.inputList.clone();
+    for (int i = 0; i < this.size; i++) {
+      // swapping random indexes
+      int rand = (int) Math.random() * this.size;
+      this.interList[i] = this.inputList[rand];
+      this.interList[rand] = this.inputList[i];
+    }
+  }
 }
