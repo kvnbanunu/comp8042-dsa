9e6ece939e6b7932e40179a01ae8b112b7c50400
kvnbanunu <kvnbanunu@gmail.com>
2026-02-02 22:31:27 -0800

    lab2: Completed Stack and parentheses checker

--- lab2/ArrayListStack.java
+++ lab2/ArrayListStack.java
@@ -5,7 +5,10 @@
  * Kevin Nguyen
  * A00955925
  */
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface Stack<T> {
   /** Tests if this stack is empty. */
@@ -25,8 +28,18 @@ interface Stack<T> {
 }
 
 public class ArrayListStack<T> implements Stack<T> {
-  private ArrayList<T> list = new ArrayList<T>();
-  private int length = 0;
+  private ArrayList<T> list;
+  private int length;
+
+  public ArrayListStack() {
+    list = new ArrayList<T>();
+    length = 0;
+  }
+
+  public ArrayListStack(int n) {
+    list = new ArrayList<>(n);
+    length = n;
+  }
 
   @Override
   public boolean empty() {
@@ -57,5 +70,70 @@ public class ArrayListStack<T> implements Stack<T> {
     length++;
   }
 
-  public static void main(String[] args) {}
+  record Parenthesis(char p, int line) {}
+
+  public static void validParentheses(String fileName) {
+    File file = new File(fileName);
+    boolean res = true;
+    ArrayListStack<Parenthesis> stack = new ArrayListStack<>();
+    int currentLine = 0;
+
+    try (Scanner scan = new Scanner(file)) {
+      while (scan.hasNextLine() && res) {
+        String line = scan.nextLine();
+        currentLine++;
+        for (int i = 0; i < line.length(); i++) {
+          char c = line.charAt(i);
+          if (c == '(' || c == '{' || c == '[') {
+            stack.push(new Parenthesis(c, currentLine));
+          } else if (c == ')' || c == '}' || c == ']') {
+            Parenthesis check = stack.pop();
+            if (check == null
+                || (c == ')' && check.p != '(')
+                || (c == '}' && check.p != '{')
+                || (c == ']' && check.p != '[')) {
+              res = false;
+              P("Invalid closing parenthesis: " + c + " at line " + currentLine);
+              break;
+            }
+          }
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      e.printStackTrace();
+    }
+
+    if (!stack.empty() && res) {
+      res = false;
+      while (!stack.empty()) {
+        Parenthesis curr = stack.pop();
+        P(
+            "Opening parenthesis: "
+                + curr.p()
+                + " at line "
+                + curr.line()
+                + " missing closing parenthesis");
+      }
+    }
+
+    if (res) {
+      P("Valid parentheses");
+    }
+  }
+
+  public static void main(String[] args) {
+    P("Task1");
+    validParentheses("StackParenthesesTask1.java");
+    P("\nTask2");
+    validParentheses("StackParenthesesTask2.java");
+    P("\nTask3");
+    validParentheses("StackParenthesesTask3.java");
+    P("\nTask4");
+    validParentheses("StackParenthesesTask4.java");
+  }
+
+  public static void P(String s) {
+    System.out.println(s);
+  }
 }
